035ab033f3ba65bd2a02dcd393c790f34fff8e0b
Jaunty <jaunty5899@gmail.com>
2023-04-29 15:13:59 +0530

    navbar complete

--- script.js
+++ script.js
@@ -5,26 +5,23 @@ let distanceObj = [];
 // pushing {left & width} into array at Load
 nav_btn.forEach((e) => {
   distanceObj.push({
-    left: Math.ceil(
-      (e.offsetLeft / document.querySelector("html").clientWidth) * 100
-    ),
-    width: Math.ceil(
-      (e.offsetWidth / document.querySelector("html").clientWidth) * 100
-    ),
+    left: e.offsetLeft,
+    width: e.offsetWidth,
   });
 });
 
 // setting starting width for highlighter
 const startSetter = () => {
-  nav_btn_active.style.width = `${distanceObj[0].width}%`;
+  nav_btn_active.style.width = `${distanceObj[1].width}px`;
+  nav_btn_active.style.left = `${distanceObj[1].left}px`;
 };
 startSetter();
 
 // adding listeners to nav buttons on load
 for (let e in distanceObj) {
   nav_btn[e].addEventListener("click", () => {
-    nav_btn_active.style.left = `${distanceObj[e].left}%`;
-    nav_btn_active.style.width = `${distanceObj[e].width}%`;
+    nav_btn_active.style.left = `${distanceObj[e].left}px`;
+    nav_btn_active.style.width = `${distanceObj[e].width}px`;
     nav_btn[e].classList.add("highlight");
     for (let i in distanceObj) {
       if (i != e) {
