7fd3feee9efd608bb70cd3738b36757df6f969e9
Jaunty <jaunty5899@gmail.com>
2023-05-01 12:39:43 +0530

    tab correlation w/menus

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const nav_btn = document.querySelectorAll(".nav-btn");
 const nav_btn_active = document.querySelector(".nav-btn-active");
 const menu_btn = document.querySelector(".menu");
-const menu_for_chats = document.querySelector(".chats-sub-menu");
+const chats_menu = document.querySelector(".chats-sub-menu");
+const status_menu = document.querySelector(".status-sub-menu");
+const calls_menu = document.querySelector(".calls-sub-menu");
+
 let distanceObj = [];
+let selected = nav_btn[1];
 
 // pushing {left & width} into array at Load
 nav_btn.forEach((e) => {
@@ -30,13 +34,35 @@ for (let e in distanceObj) {
         nav_btn[i].classList.remove("highlight");
       }
     }
+    if (nav_btn[e].classList.contains("highlight")) selected = nav_btn[e];
   });
 }
 
+// toggle for chats,status,calls&groups sub-menu
+const menu_toggle = () => {
+  if (selected.classList.contains("chats")) {
+    if (chats_menu.classList.contains("hide"))
+      chats_menu.classList.remove("hide");
+    else chats_menu.classList.add("hide");
+  }
+
+  if (selected.classList.contains("status")) {
+    if (status_menu.classList.contains("hide"))
+      status_menu.classList.remove("hide");
+    else status_menu.classList.add("hide");
+  }
+
+  if (
+    selected.classList.contains("calls") ||
+    selected.classList.contains("groups")
+  ) {
+    if (calls_menu.classList.contains("hide"))
+      calls_menu.classList.remove("hide");
+    else calls_menu.classList.add("hide");
+  }
+};
+
 // adding listener to nav menu button for chats tab
-menu_btn.addEventListener("click", (e) => {
-  console.log("this isc;alled");
-  if (menu_for_chats.classList.contains("hide"))
-    menu_for_chats.classList.remove("hide");
-  else menu_for_chats.classList.add("hide");
+menu_btn.addEventListener("click", () => {
+  menu_toggle();
 });
